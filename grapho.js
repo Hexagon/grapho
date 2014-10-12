0c1be5abfc642c9f2bc8694988f7b799a8355955
Hexagon <robinnilsson@gmail.com>
2014-10-12 18:47:33 +0200

    Minor fixes

--- grapho.js
+++ grapho.js
@@ -35,7 +35,9 @@
 
 	function merge (target, source, recurse) {
 		// Added to always get a true deep copy
-		if(recurse === undefined) target = merge({},target,true);
+		if(recurse === undefined) { 
+			target = merge({},target,true);
+		}
 
 		var name;
 	
@@ -101,7 +103,7 @@
 			max: 'auto',
 			scale: false,
 			name: undefined,
-			font: '10px Droid Sans',
+			font: '12px Droid Sans',
 			continouos: true,
 			majorTickHeight: 3,
 			minorTickHeight: 2,
@@ -431,8 +433,6 @@
 
 				px,
 				py,
-				bt, // Bar top margin
-				bb, // Bar bottom margin
 				bh, // Bar height
 
 				center = yAxis.center;
@@ -445,11 +445,9 @@
 
 					pxp = xAxis.continouos ? (point[0] - xAxis.minVal) / (xAxis.maxVal - xAxis.minVal) : xAxis.values.indexOf(parseFloat([point[0]])) / xAxis.values.length;
 
-					bt = (point[1] <= center) ? center : point[1];
-					bb = (point[1] > center) ? center : point[1];
 					px = round(padding[1] + margin + barSpacing / 2 + (pxp * innerWidth));
-					py = round(padding[0] + margin + innerHeight - (bt - min) / (max - min) * innerHeight);
-					bh = round(margin + innerHeight - (bb - min) / (max - min) * innerHeight) - py;
+					py = round(padding[0] + margin + innerHeight - (((point[1] <= center) ? center : point[1]) - min) / (max - min) * innerHeight);
+					bh = round(margin + innerHeight - (((point[1] > center) ? center : point[1]) - min) / (max - min) * innerHeight) - py;
 
 					ctx.fillRect(px, py, barWidth, bh);
 				}
@@ -474,8 +472,7 @@
 				margin,
 				padding = [], 	// 0 = top, 1 = left, 2 = right, 3 = bottom
 				used = [],		// Temprorary storage for space used, same as above
-				temp,			// Temporary storage
-				text_dimensions;		
+				temp;			// Temporary storage
 
 			// Clear canvas before drawing
 			this.ctx.clearRect(0, 0, this.w, this.h);
@@ -486,21 +483,23 @@
 			i = 0;
 			while ((dataset = this.datasets[i++])) {
 				axises = [[this.yAxises[dataset.y.axis],1,2],[this.xAxises[dataset.x.axis],3,0]];
-				for (axis in axises) {
-					axis = axises[axis];
+				for (j = 0; j < axises.length; j++) {
+					axis = axises[j];
 					if (axis[0].scale) {
 						temp = (axis[0].majorTickHeight > axis[0].minorTickHeight) ? axis[0].majorTickHeight : axis[0].minorTickHeight + 2;
-						if (axis[0].axis % 2) 
+						if (axis[0].axis % 2) {
 							padding[axis[1]] += temp;
-						else 
+						} else {
 							padding[axis[2]] += temp;
+						}
 					}
 					if (axis[0].name) {
 						temp = parseInt(axis[0].font.split(' ')[0].replace('px', ''))+2;
-						if (axis[0].axis % 2)
+						if (axis[0].axis % 2) {
 							padding[axis[1]] += temp;
-						else
+						} else {
 							padding[axis[2]] += temp;
+						}
 					}
 				}
 
@@ -549,6 +548,33 @@
 					this.ctx.restore();
 				}
 
+				if (xAxis.gridLines) {
+					this.ctx.lineWidth = 1;
+					this.ctx.strokeStyle = '#666666';
+					for (j=0; j<=xSteps; j++) {
+						x = j/xSteps;
+						if (xAxis.scale) {
+							this.ctx.beginPath();
+							this.ctx.moveTo(Math.round(padding[1]+1+x*(this.w-padding[1]-padding[2]))+0.5,padding[0]+0.5);
+							this.ctx.lineTo(Math.round(padding[1]+1+x*(this.w-padding[1]-padding[2]))+0.5,this.h-padding[3]-0.5);
+							this.ctx.stroke();
+						}
+					}
+				}
+				if (yAxis.gridLines) {
+					this.ctx.lineWidth = 1;
+					this.ctx.strokeStyle = '#666666';
+					for (j=0; j<=ySteps; j++) {
+						x = j/ySteps;
+						if (yAxis.scale) {
+							this.ctx.beginPath();
+							this.ctx.moveTo(padding[1]+0.5,Math.floor(this.h-padding[3]-x*(this.h-padding[0]-padding[3]))+0.5);
+							this.ctx.lineTo(this.w-padding[2]+0.5,Math.floor(this.h-padding[3]-x*(this.h-padding[0]-padding[3]))+0.5);
+							this.ctx.stroke();
+						}
+					}
+				}
+
 				if (xAxis.scale) {
 					// Primary X
 					if (dataset.x.axis % 2) {
@@ -584,20 +610,6 @@
 					}
 				}
 
-				if (xAxis.gridLines) {
-					this.ctx.lineWidth = 1;
-					this.ctx.strokeStyle = '#666666';
-					for (j=0; j<=xSteps; j++) {
-						x = j/xSteps;
-						if (xAxis.scale) {
-							this.ctx.beginPath();
-							this.ctx.moveTo(Math.round(padding[1]+1+x*(this.w-padding[1]-padding[2]))+0.5,padding[0]+0.5);
-							this.ctx.lineTo(Math.round(padding[1]+1+x*(this.w-padding[1]-padding[2]))+0.5,this.h-padding[3]-0.5);
-							this.ctx.stroke();
-						}
-					}
-				}
-
 				if (yAxis.scale) {
 					if (dataset.y.axis % 2) {
 						this.ctx.beginPath();
@@ -608,12 +620,10 @@
 						this.ctx.stroke();
 						for (j=0; j<=ySteps; j++) {
 							x = j/ySteps;
-							if (yAxis.scale) {
-								this.ctx.beginPath();
-								this.ctx.moveTo(padding[1]-0.5-1,this.h-padding[3]-0.5-x*(this.h-padding[0]-padding[3]));
-								this.ctx.lineTo(padding[1]-0.5-1-yAxis.majorTickHeight,this.h-padding[3]-0.5-x*(this.h-padding[0]-padding[3]));
-								this.ctx.stroke();
-							}
+							this.ctx.beginPath();
+							this.ctx.moveTo(padding[1]-0.5-1,this.h-padding[3]-0.5-x*(this.h-padding[0]-padding[3]));
+							this.ctx.lineTo(padding[1]-0.5-1-yAxis.majorTickHeight,this.h-padding[3]-0.5-x*(this.h-padding[0]-padding[3]));
+							this.ctx.stroke();
 						}
 					} else {
 						this.ctx.beginPath();
@@ -633,19 +643,6 @@
 					}
 				}
 
-				if (yAxis.gridLines) {
-					this.ctx.lineWidth = 1;
-					this.ctx.strokeStyle = '#666666';
-					for (j=0; j<=ySteps; j++) {
-						x = j/ySteps;
-						if (yAxis.scale) {
-							this.ctx.beginPath();
-							this.ctx.moveTo(padding[1]+0.5,Math.floor(this.h-padding[3]-x*(this.h-padding[0]-padding[3]))+0.5);
-							this.ctx.lineTo(this.w-padding[2]+0.5,Math.floor(this.h-padding[3]-x*(this.h-padding[0]-padding[3]))+0.5);
-							this.ctx.stroke();
-						}
-					}
-				}
 			}
 
 			// Draw charts
